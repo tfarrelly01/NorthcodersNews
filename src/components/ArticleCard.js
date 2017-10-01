180a481d26dbbf1c869dedb051ec0dc6c5a76748
Tony Farrelly <tfarrelly01@gmail.com>
2017-10-01 15:33:49 +0100

    ArticleCard component refactored

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -2,25 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 // import 'bulma/css/bulma.css';
 
-const ArticleCard = function (props) {
-  return (
-    <div className='box'>
-      <article className='media'>
-        <div className='media-left'>
-          <p>Upvotes:</p>
-          {props.votes}
-          <h3>comments</h3>
-          {props.comments}
+const ArticleCard = (props) => (
+  <div className='box'>
+    <article className='media'>
+      <div className='media-left'>
+        <p>Upvotes:</p>
+        {props.votes}
+        <h3>comments</h3>
+        {props.comments}
+      </div>
+      <div className='media-content'>
+        <div className='content'>
+          <h3 className='title is-3'>{props.title}</h3>
         </div>
-        <div className='media-content'>
-          <div className='content'>
-            <h3 className='title is-3'>{props.title}</h3>
-          </div>
-        </div>
-      </article>
-    </div>
-  );
-};
+      </div>
+    </article>
+  </div>
+);
+
 
 ArticleCard.propTypes = {
   title: PropTypes.string.isRequired,
