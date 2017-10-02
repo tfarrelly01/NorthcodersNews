6cf88c4ff6ebed6eaa51a91c31e660e7d94e0e2b
Tony Farrelly <tfarrelly01@gmail.com>
2017-10-02 12:50:46 +0100

    [TopicArticles] component renders all articles for selected topic

--- src/__test__/TopicArticles.spec.js
+++ src/__test__/TopicArticles.spec.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import store from '../store/store';
+
 import TopicArticles from '../components/TopicArticles';
 
-test('TopicArticles renders correctly', () => {
-  const component = shallow(<TopicArticles />);
-  expect(component).toMatchSnapshot();
+describe('TopicArticles Component', () => {
+  test('TopicArticles renders correctly', () => {
+    const component = shallow(
+      <Provider store={store}>
+        <TopicArticles />
+      </Provider>
+    );
+    expect(component).toMatchSnapshot();
+  });
 });
--- src/components/TopicArticles.js
+++ src/components/TopicArticles.js
@@ -1,5 +1,68 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { fetchUsers, fetchTopicArticles } from '../actions/asyncActions';
+import ArticleCard from './ArticleCard';
 
-const TopicArticles = () => <h1>Topic Articles</h1>;
+export class TopicArticles extends React.Component {
+	componentDidMount() {
+		this.props.fetchUsers();
+		this.props.fetchTopicArticles(this.props.match.params.topic_slug);
+	}
 
-export default TopicArticles;
+	componentWillReceiveProps(nextProps) {
+		if (this.props.match.params.topic_slug !== nextProps.match.params.topic_slug) {
+			this.props.fetchTopicArticles(nextProps.match.params.topic_slug);
+		}
+	}
+
+	render() {
+    return (
+      <div>
+        {this.props.articles.sort((a, b) => b.votes - a.votes)
+          .map(article => {
+            let userProfile = this.props.users.find(user => user.username === article.created_by);
+            return (
+              <ArticleCard
+                key={article._id}
+                title={article.title}
+                createdBy={article.created_by}
+                votes={article.votes}
+                comments={article.comments}
+                avatarUrl={userProfile.avatar_url}
+              />
+            );
+          }
+				)}
+      </div>
+		);
+	}
+}
+
+function mapStateToProps(state) {
+	return {
+		users: state.users,
+		articles: state.articles
+	};
+}
+
+function mapDispatchToProps(dispatch) {
+	return {
+		fetchUsers: () => {
+			dispatch(fetchUsers());
+		},
+		fetchTopicArticles: id => {
+			dispatch(fetchTopicArticles(id));
+		}
+	};
+}
+
+TopicArticles.propTypes = {
+	users: PropTypes.array.isRequired,
+	articles: PropTypes.array.isRequired,
+	match: PropTypes.object.isRequired,
+	fetchUsers: PropTypes.func.isRequired,
+	fetchTopicArticles: PropTypes.func.isRequired
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TopicArticles);
